bf0d0488d26fe9ace58b995ca8ea2e4f302b7bab
averroes-alberuni <averroes-alberuni@users.noreply.github.com>
2017-05-19 00:04:13 +0530

    commiting

---
+++ AngularJs-Tutorial/Scripts/Script.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+    var employee = {
+        firstName: "David",
+        lastName: "Hastings",
+        gender: "Male"
+    };
+    $scope.employee = employee;
+
+});
